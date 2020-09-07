f86c867bd709cedda8843111c471020b21918c85
payer <payermann@gmail.com>
2020-09-07 16:35:33 +0300

    update: add product detail

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -1,41 +1,58 @@
 import React, { Component } from "react";
-
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import ProductsService from "./ProductsService";
-
+import IconButton from "@material-ui/core/IconButton";
 const productsService = new ProductsService();
 
 class ProductDetail extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: "",
+      price: "",
+      specification: "",
+      image: "",
+    };
   }
 
   componentDidMount() {
+    let self = this;
     const {
       match: { params },
     } = this.props;
     if (params && params.pk) {
       productsService.getProduct(params.pk).then((c) => {
-        this.refs.name.value = c.name;
-        this.refs.price.value = c.price;
-        this.refs.specification.value = c.specification;
-        this.refs.image.value = c.image;
+        self.setState({
+          name: c.name,
+          price: c.price,
+          specification: c.specification,
+          image: c.image,
+        });
       });
     }
   }
 
   render() {
     return (
-      <div className="form-group">
-        <label>Name :</label>
-
-        <input className="form-control" ref="name" readOnly />
-
-        <label>price:</label>
-        <input className="form-control" ref="price" readOnly />
-
-        <label>specification:</label>
-        <input className="form-control" ref="specification" readOnly />
-      </div>
+      // <div className="form-group">
+        <div className="item">
+                <div className="product-img">
+                  <img alt={this.state.name} src={this.state.image} />
+                </div>
+                <div className="product">
+                  <h1 id="product-name">{this.state.name}</h1>
+                  <h4 id="product-description">{this.state.specification}</h4>
+                </div>
+              
+              <div className="price-add">
+                <h5 id="product-price">{this.state.price}₽</h5>
+                <IconButton color="primary" aria-label="add to shopping cart">
+                  <AddShoppingCartIcon />
+                </IconButton>
+              </div>
+        </div>      
+      // </div>
+      
     );
   }
 }
