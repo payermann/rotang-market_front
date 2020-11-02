2b2dca1645ffede1ecac5ad1deb6d83f46f0ffc3
payer <payermann@gmail.com>
2020-11-02 12:27:31 +0300

    feat(redux): сделана простейшая логика

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { createStore } from 'redux';
+
+function counter(state = { value: 0 }, action) {
+  switch (action.type) {
+    case 'counter/incremented':
+      return { value: state.value + 1 }
+    case 'counter/decremented':
+      return { value: state.value - 1 }
+    default:
+      return state
+  }
+}
+let store = createStore(counter)
+
+store.subscribe(() => console.log(store.getState().value))
+store.dispatch({ type: 'counter/incremented'})
 
 ReactDOM.render(
   <App />,
